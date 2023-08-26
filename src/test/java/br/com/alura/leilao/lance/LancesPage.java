1accf47e47dad2dc85f02665ef00e1069c52e450
Thiago Cavalcanti <tcavalcanti.tc@gmail.com>
2023-08-26 02:34:42 -0300

    Teste de lance menor que o valor mínimo

--- src/test/java/br/com/alura/leilao/lance/LancesPage.java
+++ src/test/java/br/com/alura/leilao/lance/LancesPage.java
@@ -2,7 +2,10 @@ package br.com.alura.leilao.lance;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.math.BigDecimal;
 
 public class LancesPage {
 
@@ -43,6 +46,18 @@ public class LancesPage {
 	public boolean isErroLanceMenorQue10Centavos() {
 		String pageSource = this.browser.getPageSource();
 		return pageSource.contains("deve ser maior que ou igual a 0.1");
+	}
+
+	public BigDecimal obterValorInicial() {
+		WebElement valorInicialElement = this.browser.findElement(By.id("valorInicial"));
+		String valorInicialText = valorInicialElement.getText().replaceAll("[^0-9.,]", ""); // Remove caracteres não numéricos
+		valorInicialText = valorInicialText.replace(",", "."); // Substitui vírgulas por pontos para o formato BigDecimal
+		return new BigDecimal(valorInicialText);
+	}
+
+	public boolean isLanceInvalido() {
+		String pageSource = this.browser.getPageSource();
+		return pageSource.contains("Lance invalido");
 	}
 
 }
--- src/test/java/br/com/alura/leilao/lance/LancesTest.java
+++ src/test/java/br/com/alura/leilao/lance/LancesTest.java
@@ -4,6 +4,8 @@ import br.com.alura.leilao.leiloes.LeiloesPage;
 import br.com.alura.leilao.login.LoginPage;
 import org.junit.jupiter.api.*;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -33,5 +35,22 @@ public class LancesTest {
 		assertTrue(paginaDeLances.isErroLanceMenorQue10Centavos());
 		assertTrue(paginaDeLances.isPaginaAtual()); // Falha: fez requisição para /lances e não houve redirect para retornar pro leilao
 	}
+
+	@Test
+	public void naoDeveriaDarLanceMenorQueOValorMinimo() {
+		BigDecimal lanceMenos5Centavos = paginaDeLances.obterValorInicial().subtract(new BigDecimal("0.05"));
+
+		paginaDeLances.preencherValor(lanceMenos5Centavos.toString());
+		paginaDeLances.submeterLance();
+
+		assertTrue(paginaDeLances.isLanceInvalido()); // Falha: está aceitando valores menores que o mínimo
+		assertTrue(paginaDeLances.isPaginaAtual());
+	}
+
+	/*
+	O ideal seria realizar mais dois testes, um com valor igual ao mínimo e outro com valor superior.
+	Entretanto, como o usuário não pode dar dois lances repetidos, dificulta o teste pois tem que limpar a base,
+	logar com outro usuário ou mockar o banco (separar as sessões).
+	*/
 
 }
